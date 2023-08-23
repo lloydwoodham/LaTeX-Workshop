6ba94d15a91020161e9611a6b299dfd4570d9263
Sourcery AI <>
2023-08-23 10:18:28 +0000

    'Refactored by Sourcery'

--- dev/spaces.py
+++ dev/spaces.py
@@ -7,12 +7,10 @@ cmd_files = cwd.joinpath('../data/packages').glob('*.json')
 all_cmds = {}
 for f in cmd_files:
     f_cmds = json.load(open(f, encoding='utf8'))
-    all_cmds.update(f_cmds.cmds)
+    all_cmds |= f_cmds.cmds
 
-cmds_with_spaces = {}
-for key in all_cmds:
-    if key.find(' ') >= 0:
-        cmds_with_spaces[key] = all_cmds[key]
-
-for entry in [(k, cmds_with_spaces[k]['package']) for k in cmds_with_spaces.keys()]:
+cmds_with_spaces = {
+    key: all_cmds[key] for key in all_cmds if key.find(' ') >= 0
+}
+for entry in [(k, cmds_with_spaces[k]['package']) for k in cmds_with_spaces]:
     print(f'{entry[1]}: {entry[0]}')
